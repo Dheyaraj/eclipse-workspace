e1c6bf5253aedda98e6c482e9f3e25de220c10d2
Dheyaraj <159921025+Dheyaraj@users.noreply.github.com>
2024-02-15 10:46:12 +0530

    insertion sort program

---
+++ Sorting/src/insertionSort.java
@@ -0,0 +1,13 @@
+
+public class insertionSort {
+
+	public insertionSort() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sorting");
+	}
+
+}
